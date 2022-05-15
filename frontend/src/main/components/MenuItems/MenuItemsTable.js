0243585fa76ca948452a1f212ed9e566aca3ad16
Kevin Lai <clai@ucsb.edu>
2022-05-15 13:28:20 -0700

    KL - added Delete Column to MenuItemTable and new tests for table and index page

--- frontend/src/main/components/MenuItems/MenuItemsTable.js
+++ frontend/src/main/components/MenuItems/MenuItemsTable.js
@@ -1,8 +1,19 @@
-import OurTable/*, { ButtonColumn }*/ from "main/components/OurTable";
-// import { useBackendMutation } from "main/utils/useBackend";
-// import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
 // import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
+
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/UCSBDiningCommonsMenuItem",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
 
 export default function MenuItemsTable({ menuItems, currentUser }) {
 
@@ -13,15 +24,15 @@ export default function MenuItemsTable({ menuItems, currentUser }) {
     // }
 
     // Stryker disable all : hard to test for query caching
-    // const deleteMutation = useBackendMutation(
-    //     cellToAxiosParamsDelete,
-    //     { onSuccess: onDeleteSuccess },
-    //     ["/api/ucsbdates/all"]
-    // );
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/UCSBDiningCommonsMenuItem/all"]
+    );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     // "diningCommonsCode": "string",
     // "id": 0,
@@ -47,12 +58,12 @@ export default function MenuItemsTable({ menuItems, currentUser }) {
         }
     ];
 
-    // const testid = "MenuItemsTable";
+    const testid = "MenuItemsTable";
 
     const columnsIfAdmin = [
         ...columns,
         // ButtonColumn("Edit", "primary", editCallback, testid),
-        // ButtonColumn("Delete", "danger", deleteCallback, testid)
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
     ];
 
     const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
@@ -60,6 +71,6 @@ export default function MenuItemsTable({ menuItems, currentUser }) {
     return <OurTable
         data={menuItems}
         columns={columnsToDisplay}
-        testid={"MenuItemsTable"}
+        testid={testid}
     />;
 }; 
