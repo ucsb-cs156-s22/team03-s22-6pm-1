8d09acc19f5954fe5fadf857cc755df7f5fb05a8
Kevin Lai <clai@ucsb.edu>
2022-05-14 23:15:38 -0700

    KL - added menu item component to storyboard and filled out fixtures

---
+++ frontend/src/fixtures/menuItemsFixtures.js
@@ -0,0 +1,31 @@
+const menuItemsFixtures = {
+    oneMenuItem: {
+        "diningCommonsCode": "de-la-guerra",
+        "id": 0,
+        "name": "Taco Pizza",
+        "station": "Pizza"
+    },
+    threeMenuItems: [
+        {
+            "diningCommonsCode": "de-la-guerra",
+            "id": 0,
+            "name": "Taco Pizza",
+            "station": "Pizza"
+        },
+        {
+            "diningCommonsCode": "ortega",
+            "id": 1,
+            "name": "Mayonnaise (v)",
+            "station": "Condiments"
+        },
+        {
+            "diningCommonsCode": "carrillo",
+            "id": 2,
+            "name": "Meat Sauce",
+            "station": "Pasta"
+        } 
+    ]
+};
+
+
+export { menuItemsFixtures };
