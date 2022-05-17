6a64fa28e984c72115383480f27ea050f05bfe88
Srijit Boinapally <srijit@umail.ucsb.edu>
2022-05-16 22:06:56 -0700

    created the table for the organization page

--- frontend/src/main/components/Organizations/OrganizationsTable.js
+++ frontend/src/main/components/Organizations/OrganizationsTable.js
@@ -2,9 +2,9 @@ import OurTable from "main/components/OurTable";
 // import { useBackendMutation } from "main/utils/useBackend";
 // import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
 // import { useNavigate } from "react-router-dom";
-import { hasRole } from "main/utils/currentUser";
+//import { hasRole } from "main/utils/currentUser";
 
-export default function OrganizationsTable({ organizations, currentUser }) {
+export default function OrganizationsTable({ organizations, _currentUser }) {
 
     //const navigate = useNavigate();
 
@@ -21,38 +21,43 @@ export default function OrganizationsTable({ organizations, currentUser }) {
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+   // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+
 
     const columns = [
         {
-            Header: 'id',
-            accessor: 'id', // accessor is the "key" in the data
+            Header: 'orgCode',
+            accessor: 'orgCode', // accessor is the "key" in the data
         },
         {
-            Header: 'QuarterYYYYQ',
-            accessor: 'quarterYYYYQ',
+            Header: 'orgTranslationShort',
+            accessor: 'orgTranslationShort',
         },
         {
-            Header: 'Name',
-            accessor: 'name',
+            Header: 'orgTranslation',
+            accessor: 'orgTranslation',
         },
         {
-            Header: 'Date',
-            accessor: 'localDateTime',
+            Header: 'Is inactive?',
+            accessor: 'inactive',
+            accessor: (row, _rowIndex) => String(row.inactive)
         }
     ];
 
-    const columnsIfAdmin = [
-        ...columns,
-        ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
-        ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
-    ];
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
+    //     ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
+    // ];
 
-    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    const columnsToDisplay = columns;
 
     return <OurTable
-        data={dates}
+        data={organizations}
         columns={columnsToDisplay}
-        testid={"UCSBDatesTable"}
+        testid={"OrganizationsTable"}
     />;
 };
---
+++ frontend/src/stories/components/Organizations/OrganizationsTable.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import OrganizationsTable from "main/components/Organizations/OrganizationsTable";
+import { organizationsFixtures } from 'fixtures/organizationsFixtures';
+
+export default {
+    title: 'components/Organizations/OrganizationsTable',
+    component: OrganizationsTable
+};
+
+const Template = (args) => {
+    return (
+        <OrganizationsTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    organizations: []
+};
+
+export const threeOrganizations = Template.bind({});
+
+ThreeDates.args = {
+    organizations: organizationsFixtures.threeOrganizations
+};
+
+
