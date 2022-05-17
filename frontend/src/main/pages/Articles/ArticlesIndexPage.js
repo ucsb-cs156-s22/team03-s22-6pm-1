e4f046371d62478d3b278e396865535df9682861
Anuragh Sundar <anuraghsundar@ucsb.edu>
2022-05-17 16:43:57 -0700

    as - added dummy test files

--- frontend/src/main/pages/Articles/ArticlesIndexPage.js
+++ frontend/src/main/pages/Articles/ArticlesIndexPage.js
@@ -1,28 +1,14 @@
-import { render } from "@testing-library/react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { MemoryRouter } from "react-router-dom";
-import ArticlesIndexPage from "main/pages/Articles/ArticlesIndexPage";
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
-import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import axios from "axios";
-import AxiosMockAdapter from "axios-mock-adapter";
-
-describe("ArticlesIndexPage tests", () => {
-
-    const axiosMock =new AxiosMockAdapter(axios);
-    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-
-    const queryClient = new QueryClient();
-    test("renders without crashing", () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <ArticlesIndexPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-    });
-
-});
+export default function ArticlsIndexPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Articles</h1>
+        <p>
+          This is where the index page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
