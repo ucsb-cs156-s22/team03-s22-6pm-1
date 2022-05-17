4c84a4210369e2ce112b8711b2abbb666f32c63a
Timothy Chang <timothychang@ucsb.edu>
2022-05-17 14:34:58 -0700

    TC - fixed minor typo in MenuItemReviewController

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -23,10 +23,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-<<<<<<< HEAD
-=======
-
->>>>>>> efc3fe6 (added controller for MenuItemReview)
 import java.time.LocalDateTime;
 
 @Api(description = "MenuItemReview")
