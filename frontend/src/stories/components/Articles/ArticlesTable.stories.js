fe160015fd361c9c1161ae8c49c60375a1961cb9
Anuragh Sundar <anuraghsundar@ucsb.edu>
2022-05-18 18:13:55 -0700

    added articles table stories

---
+++ frontend/src/stories/components/Articles/ArticlesTable.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ArticlesTable from "main/components/Articles/ArticlesTable";
+import { articlesFixtures } from 'fixtures/articlesFixtures';
+
+export default {
+    title: 'components/Articles/ArticlesTable',
+    component: ArticlesTable
+};
+
+const Template = (args) => {
+    return (
+        <ArticlesTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    articles: []
+};
+
+export const ThreeDates = Template.bind({});
+
+ThreeDates.args = {
+    articles: articlesFixtures.threeCommons
+};
