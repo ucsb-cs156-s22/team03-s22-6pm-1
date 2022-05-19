17ece866afd57accca595e1e7ebfa98ec777c60c
AadityaChannabasappa <81777074+AadityaChannabasappa@users.noreply.github.com>
2022-05-18 22:11:23 -0700

    Added testid
    Added testid

--- frontend/src/main/components/Articles/ArticlesTable.js
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -51,7 +51,7 @@ export default function ArticlesTable({ articles, currentUser }) {
         }
     ];
 
-    
+    const testid = "ArticlesTable";
 
     const columnsIfAdmin = [
         ...columns,
